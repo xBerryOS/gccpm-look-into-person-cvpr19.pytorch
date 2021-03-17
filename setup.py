fa1210106ba16942a66198a628030ffe75e30c20
kkulczak <kulczakkacper@gmail.com>
2021-03-17 16:08:07 +0100

    url added

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='gc_cpm_networks',
-    version='0.0.2',
+    version='0.0.3',
     description='Global Context Convolutional Pose Machine Network',
     author='Kacper Kulczak',
     author_email='k.kulczak@xberry.tech',
     packages=find_packages(),
+    url='https://github.com/xBerryOS/gccpm-look-into-person-cvpr19.pytorch',
     python_requires=">=3.8.*",
 )
