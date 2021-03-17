1f2c24f254d01ee9eb43dcd68a7fae6d51df01f0
kkulczak <kulczakkacper@gmail.com>
2021-03-17 15:29:53 +0100

    pacakge created

--- modules/loss.py
+++
@@ -1,8 +0,0 @@
-def l2_loss(input, target, batch_size, mask=None):
-    if mask:
-        loss = (input - target) * mask
-    else:
-        loss = (input - target)
-    loss = (loss * loss) / 2 / batch_size
-
-    return loss.sum()
