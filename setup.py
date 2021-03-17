f60e4b296f2e5c702525cb761f7d8d93ee04c3e7
kkulczak <kulczakkacper@gmail.com>
2021-03-17 15:50:12 +0100

    init files added

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='gc_cpm_networks',
-    version='0.0.1',
+    version='0.0.2',
     description='Global Context Convolutional Pose Machine Network',
     author='Kacper Kulczak',
     author_email='k.kulczak@xberry.tech',
-    packages=find_packages(include=['gc_cpm_networks']),
+    packages=find_packages(),
     python_requires=">=3.8.*",
 )
